9504239e2f76ab7e20cae31948871f1746a196d5
Suliman Alsaid <saloomi99@gmail.com>
2019-01-29 23:08:08 -0500

    Finished all of first chapter Quiz program with icon

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if(currentQuestionIndex == questions.count) {
+            currentQuestionIndex = 0
+        }
+        
+        questionLabel.text = questions[currentQuestionIndex]
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = answers[currentQuestionIndex]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
