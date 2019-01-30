ba3bc2da923096f1f8698ae23572042c86f1454d
Suliman Alsaid <saloomi99@gmail.com>
2019-01-30 13:26:30 -0500

    Finished Part 2 of the Quiz app adding in the High Point University label and extra questions

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Suliman Alsaid on 1/28/19.
 //  Copyright © 2019 Suliman Alsaid. All rights reserved.
-//
+//  High Point University
 
 import UIKit
 
@@ -15,13 +15,23 @@ class ViewController: UIViewController {
     let questions: [String] = [
         "What is 7+7?",
         "What is the capital of Vermont?",
-        "What is cognac made from?"
+        "What is cognac made from?",
+        "Who invented C++?",
+        "What is the liquid component of blood called?",
+        "How many holes are there in a Ritz cracker?",//
+        "What year did Hawaii become a US state?",
+        "What gives beer its distinctive bitter flavour?"
     ]
     
     let answers: [String] = [
         "14",
         "Montpelier",
-        "Grapes"
+        "Grapes",
+        "Bjarne Stroustrup",
+        "Plasma",
+        "7",
+        "1959",
+        "Hops"
     ]
     
     var currentQuestionIndex: Int = 0
